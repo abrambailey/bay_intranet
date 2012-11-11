dafb7ce7c6a639af152cae634e02f32b0fca204c
funkdified <funkdified@gmail.com>
2012-11-11 14:21:10 +1300

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bay::Application.config.secret_token = '409d96940b09bf64b6f92482bf31a8fd4be5c04edd4f8e3bcfb449d851f667b6f6ed230288851bafe138fba5671de6888e0a04d8c034fefff497efbd3c886de4'
